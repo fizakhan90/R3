a7bd102f5ecd53520b5e735e096fb14ca1ecf2bc
Fiza Khan <fiza1892003@gmail.com>
2025-06-28 01:38:54 +0530

    feat:learn a new skill via a chatbot using gemini

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -1,6 +1,7 @@
 // lib/services/gemini_service.dart
 import 'dart:convert';
-import 'package.flutter/foundation.dart';
+import 'package:flutter/foundation.dart';
+
 import 'package:http/http.dart' as http;
 import 'package:r3/secrets.dart'; // Import our secret API key
 
